be4fd706f28cd4c1f3e9a9f58089a936118ebb1b
skywalkeratlas <SkywalkerAtlas@users.noreply.github.com>
2018-02-18 20:30:59 +0800

    complete the code

--- sparase_autoencoder.py
+++ sparase_autoencoder.py
@@ -1,10 +1,31 @@
 import tensorflow as tf
+import numpy as np
+
 from tensorflow.examples.tutorials.mnist import input_data
+
+
+def input_fn_gen(function, features_generator, labels=None, batch_size=None):
+    classifier_x = []
+    for result in features_generator:
+        classifier_x.append(result['encoder_result'])
+    # print(classifier_x)
+    classifier_x = tf.convert_to_tensor(np.asarray(classifier_x))
+    assert function == 'train' or function == 'predict' or function == 'eval', "function must be 'train', 'predict' or 'eval'"
+    if function == 'train':
+        data_set = tf.data.Dataset.from_tensor_slices((classifier_x, labels))
+        data_set = data_set.shuffle(512).repeat().batch(batch_size)
+    elif function == 'eval':
+        data_set = tf.data.Dataset.from_tensor_slices((classifier_x, labels))
+        data_set = data_set.batch(batch_size)
+    else:
+        data_set = tf.data.Dataset.from_tensor_slices(classifier_x).batch(batch_size)
+
+    return data_set
+
 
 
 def input_fn(function, features, labels=None, batch_size=None):
     """Custom input function to input data for training, evaluation or prediction step
-
 
     Args:
         function: selected function, must be train, predict or eval
@@ -15,65 +36,74 @@ def input_fn(function, features, labels=None, batch_size=None):
     Returns:
         a Dataset
     """
-    features = dict(features)
-    assert function == 'train' or 'predict' or 'eval', "function must be 'train', 'predict' or 'eval'"
+    # features = dict(features)
+    assert function == 'train' or function == 'predict' or function == 'eval', "function must be 'train', 'predict' or 'eval'"
     if function == 'train':
         data_set = tf.data.Dataset.from_tensor_slices((features, labels))
-        data_set = data_set.shuffle(1024).repeat().batch(batch_size)
+        data_set = data_set.shuffle(512).repeat().batch(batch_size)
     elif function == 'eval':
         data_set = tf.data.Dataset.from_tensor_slices((features, labels))
+        data_set = data_set.batch(batch_size)
     else:
-        data_set = tf.data.Dataset.from_tensor_slices(features)
+        data_set = tf.data.Dataset.from_tensor_slices(features).batch(batch_size)
 
     return data_set
 
 
 def sparase_autoencoder(features, labels, mode, params):
     """some note"""
-    net = tf.feature_column.input_layer(features ,params['feature_columns'])
+    net = tf.layers.dense(features, features.shape[1], activation=None, name='INPUT')
 
     # define encoder
-    for units in params['encoder_units']:
-        encoder_net = tf.layers.dense(net, units, activation=tf.nn.sigmoid)
+    encoder_net = net
+    for index, units in enumerate(params['encoder_units']):
+        encoder_net = tf.layers.dense(encoder_net, units, activation=tf.nn.sigmoid, name='encoder_'+str(index))
+
+    # Define encoder output
+    en_result = tf.layers.dense(encoder_net, params['encoder_result_units'], activation=tf.nn.sigmoid, name='ENCODER_OUTPUT')
 
     # define decoder
-    for units in params['decoder_units']:
-        decoder_net = tf.layers.dense(encoder_net, units, activation=tf.nn.sigmoid)
+    decoder_net = en_result
+    for index, units in enumerate(params['decoder_units']):
+        decoder_net = tf.layers.dense(decoder_net, units, activation=tf.nn.sigmoid, name='decoder_'+str(index))
+
+    # Define decoder result
+    decoder_result = tf.layers.dense(decoder_net, features.shape[1], activation=tf.nn.sigmoid, name='DECODER_OUTPUT')
 
     # compute predictions
     if mode == tf.estimator.ModeKeys.PREDICT:
         predictions = {
-            'encoder_result': encoder_net
+            'encoder_result': en_result
         }
         return tf.estimator.EstimatorSpec(mode, predictions)
 
     # compute loss
-    loss = tf.losses.mean_squared_error(labels, decoder_net)
+    loss = tf.losses.mean_squared_error(labels=labels, predictions=decoder_result)
 
     # compute evaluation metrics
-    accuracy = tf.metrics.accuracy(labels, predictions=decoder_net, name='accuracy_op')
-    metrics = {'accuracy', accuracy}
-    tf.summary.scalar('accuracy', accuracy[1])
+    # accuracy = tf.metrics.accuracy(labels=labels, predictions=decoder_result, name='accuracy_op')
+    # metrics = {'accuracy': accuracy}
+    # tf.summary.scalar('accuracy', accuracy[1])
 
     if mode == tf.estimator.ModeKeys.EVAL:
-        return tf.estimator.EstimatorSpec(mode, loss, eval_metric_ops=metrics)
+        return tf.estimator.EstimatorSpec(mode, loss=loss)
 
     assert mode == tf.estimator.ModeKeys.TRAIN
 
     optimizer = tf.train.AdamOptimizer()
     train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
-    return tf.estimator.EstimatorSpec(mode, loss, train_op=train_op)
+    return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
 
 
 def pic_classifier(features, labels, mode, params):
     """some note"""
-    net = tf.feature_column.input_layer(features, params['feature_column'])
+    net = tf.layers.dense(features, features.shape[1], activation=None, name='INPUT')
 
     # define classifier network
-    for units in params['units']:
-        net = tf.layers.dense(net, units, activation=tf.nn.sigmoid)
+    for index, units in enumerate(params['units']):
+        net = tf.layers.dense(net, units, activation=tf.nn.sigmoid, name='hidden_'+str(index))
 
-    logits = tf.layers.dense(net, params['n_class'], activation=None)
+    logits = tf.layers.dense(net, params['n_classes'], activation=None, name='logits')
 
     predicted_class = tf.argmax(logits, 1)
     if mode == tf.estimator.ModeKeys.PREDICT:
@@ -83,25 +113,91 @@ def pic_classifier(features, labels, mode, params):
         }
         return tf.estimator.EstimatorSpec(mode, predictions=predictions)
 
-    loss = tf.losses.sparse_softmax_cross_entropy(labels, logits)
+    # Compute losses
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=labels))
 
+    # Compute evaluation metrics
+    labels = tf.argmax(labels, 1)
     accuracy = tf.metrics.accuracy(labels, predictions=predicted_class, name='classifier_acc_op')
-    metrics = {'accuracy', accuracy}
+    metrics = {'accuracy': accuracy}
     tf.summary.scalar('accuracy', accuracy[1])
 
     if mode == tf.estimator.ModeKeys.EVAL:
-        return tf.estimator.EstimatorSpec(mode, loss, eval_metric_ops=metrics)
+        return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=metrics)
 
     # create train op
     assert mode == tf.estimator.ModeKeys.TRAIN
 
     optimizer = tf.train.AdamOptimizer()
     train_op = optimizer.minimize(loss, global_step=tf.train.get_global_step())
-    return tf.estimator.EstimatorSpec(mode, loss, train_op=train_op)
+    return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=train_op)
 
 
-def main():
-    pass
+def main(argv):
+    mnist = input_data.read_data_sets('../MNIST_data/', one_hot=True)
+    # mnist.train.next_batch()
+
+    # Build the auto encoder
+    auto_encoder = tf.estimator.Estimator(
+        model_fn=sparase_autoencoder,
+        model_dir='../log/SAE',
+        params={
+            'encoder_units': [],
+            'encoder_result_units': 200,
+            'decoder_units': [],
+        }
+    )
+
+    # Train the model
+    auto_encoder.train(
+        input_fn=lambda : input_fn('train', mnist.train.images, mnist.train.images, batch_size=128),
+        steps=3000
+    )
+
+    # # Evaluate the model
+    # eval_result = auto_encoder.evaluate(
+    #     input_fn=lambda : input_fn(
+    #         function='eval',
+    #         features=mnist.test.images[:20],
+    #         labels=mnist.test.images[:20],
+    #         batch_size=20
+    #     )
+    # )
+
+    # Build mnist classifier
+    mnist_classifier = tf.estimator.Estimator(
+        model_fn=pic_classifier,
+        model_dir='../log/classifier',
+        params={
+            'units': [128],
+            'n_classes': 10,
+        }
+    )
+
+    # Get training input data from previous auto encoder
+    classifier_gen = auto_encoder.predict(
+        input_fn=lambda : input_fn('predict', mnist.train.images, batch_size=mnist.train.images.shape[0])
+    )
+
+
+    # Train classifier
+    mnist_classifier.train(
+        input_fn=lambda : input_fn_gen('train', classifier_gen, mnist.train.labels, batch_size=128),
+        steps=3000
+    )
+
+    # Get test data
+    classifier_gen_test = auto_encoder.predict(
+        input_fn=lambda :input_fn('predict', mnist.test.images, batch_size=mnist.train.images.shape[0])
+    )
+
+    # Evaluate classifier
+    classifier_eval_result = mnist_classifier.evaluate(
+        input_fn=lambda : input_fn_gen('eval', classifier_gen_test, mnist.test.labels, mnist.test.labels.shape[0])
+    )
+
+    print('\nTest accuracy:{accuracy: 0.3f}\n'.format(**classifier_eval_result))
+
 
 if __name__ == '__main__':
     tf.logging.set_verbosity(tf.logging.INFO)
